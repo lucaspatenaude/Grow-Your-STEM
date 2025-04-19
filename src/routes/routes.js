3955319fd102a9c4b4625cb7d0a3fb001af1ed54
Lucas Patenaude <LUPA7501@colorado.edu>
2025-04-19 02:13:44 -0600

    Routes and pages for games and blog pages created

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-// Insert routes here
-// Render home page for /home route
+// ************** Home Page Routes ************** 
+
 router.get("/home", (req, res) => {
     res.render("pages/home");
 });
 
 router.get("/", (req, res) => {
     res.redirect("/home");
+});
+
+// ************** Blog Page Routes ************** 
+
+router.get("/blog", (req, res) => {
+    res.render("pages/blog");
+});
+
+// ************** Games Page Routes **************
+
+router.get("/games", (req, res) => {
+    res.render("pages/games");
 });
 
 module.exports = router;
