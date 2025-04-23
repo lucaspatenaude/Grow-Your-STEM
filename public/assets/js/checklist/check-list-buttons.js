f2b90fff80cd5433c3a5544c572ba8ffae74b6a1
Lucas Patenaude <LUPA7501@colorado.edu>
2025-04-23 05:51:09 -0600

    Add checklist feature with interactive buttons and styling

---
+++ public/assets/js/checklist/check-list-buttons.js
@@ -0,0 +1,19 @@
+document.getElementById('article-checkbox-1').addEventListener('click', () => {
+  const checkbox = document.getElementById('checkbox-1');
+  checkbox.checked = !checkbox.checked;
+});
+
+document.getElementById('toggle-checkbox-2').addEventListener('click', () => {
+  const checkbox = document.getElementById('checkbox-2');
+  checkbox.checked = !checkbox.checked;
+});
+
+document.getElementById('toggle-checkbox-3').addEventListener('click', () => {
+  const checkbox = document.getElementById('checkbox-3');
+  checkbox.checked = !checkbox.checked;
+});
+
+document.getElementById('toggle-checkbox-4').addEventListener('click', () => {
+  const checkbox = document.getElementById('checkbox-4');
+  checkbox.checked = !checkbox.checked;
+});
