9000e1df2b35567c936e85143c606b483af78775
Lucas Patenaude <LUPA7501@colorado.edu>
2025-04-29 13:13:35 -0600

    Enhance styling and structure of login and registration panes; update navbar behavior to include account portal positioning

--- public/assets/js/user-menu/show-user-menu.js
+++ public/assets/js/user-menu/show-user-menu.js
@@ -1,44 +1,55 @@
 $(document).ready(function () {
-	// When #user is clicked
-	$("#user-profile-button").click(function () {
-		$("#register-screen-container").hide();
-		// Toggle the visibility of the login container
-		$("#login-screen-container").toggle();
-	});
+    // When #user is clicked
+    $("#user-profile-button").click(function () {
+        $("#register-screen-container").hide();
+        // Toggle the visibility of the login container
+        $("#login-screen-container").toggle();
+        // Recalculate the position of the account portal
+        stickyNavbar();
+    });
 
-	$("#register-page-login-button").click(function (event) {
-		event.preventDefault(); // Prevent the default action of following the link
+    $("#register-page-login-button").click(function (event) {
+        event.preventDefault(); // Prevent the default action of following the link
 
-		$("#register-screen-container").hide();
-		$("#login-screen-container").show();
-	});
+        $("#register-screen-container").hide();
+        $("#login-screen-container").show();
+        // Recalculate the position of the account portal
+        stickyNavbar();
+    });
 });
 
 $(document).ready(function () {
-	// Listen for click event on the register button
-	$("#login-page-register-button").click(function (event) {
-		event.preventDefault(); // Prevent the default action of following the link
+    // Listen for click event on the register button
+    $("#login-page-register-button").click(function (event) {
+        event.preventDefault(); // Prevent the default action of following the link
 
-		$("#login-screen-container").hide();
-		// Show the register container
-		$("#register-screen-container").show();
-	});
+        $("#login-screen-container").hide();
+        // Show the register container
+        $("#register-screen-container").show();
+        // Recalculate the position of the account portal
+        stickyNavbar();
+    });
 });
 
 // Show the login screen if there is an alert message
 $(document).ready(function () {
-	if ($("#login-screen-container").find(".alert").length > 0) {
-			$("#login-screen-container").show();
-	}
-	else if ($("#register-screen-container").find(".alert").length > 0) {
-		$("#register-screen-container").show();
-	}
+    if ($("#login-screen-container").find(".alert").length > 0) {
+        $("#login-screen-container").show();
+        // Recalculate the position of the account portal
+        stickyNavbar();
+    } else if ($("#register-screen-container").find(".alert").length > 0) {
+        $("#register-screen-container").show();
+        // Recalculate the position of the account portal
+        stickyNavbar();
+    }
 });
 
 $(document).ready(function () {
-	// When #user is clicked
-	$("#user-profile-button").click(function () {
-		// Toggle the visibility of the login container
-		$("#account-screen-container").toggle();
-	});
+    // When #user is clicked
+    $("#user-profile-button").click(function () {
+        // Toggle the visibility of the login container
+        $("#account-screen-container").toggle();
+        // Recalculate the position of the account portal
+        stickyNavbar();
+    });
 });
