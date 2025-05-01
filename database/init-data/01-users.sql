4b45ec71c08f656a4d8e31532a232ccaa70aecf5
Lucas Patenaude <LUPA7501@colorado.edu>
2025-05-01 17:08:58 -0600

    Remove default user and task insert statements from SQL initialization scripts

--- database/init-data/01-users.sql
+++ database/init-data/01-users.sql
@@ -4,8 +4,3 @@ CREATE TABLE users (
     Password CHAR(60) NOT NULL,
     Score INT DEFAULT 0
 );
-
--- Insert a default user
-INSERT INTO users (Username, Password, Score)
-VALUES
-    ('default_user', 'hashed_password_placeholder', 0);
--- database/init-data/02-tasks.sql
+++ database/init-data/02-tasks.sql
@@ -5,16 +5,3 @@ CREATE TABLE tasks (
     Points INT NOT NULL,
     IsCompleted BOOLEAN DEFAULT FALSE
 );
-
-INSERT INTO tasks (UserID, TaskName, Points, IsCompleted)
-VALUES
-    (1, 'Read Article "How Do Tariffs Work"', 10, FALSE),
-    (1, 'Read Article "Credit and Financing Options"', 10, FALSE),
-    (1, 'Read Article "Retirement Accounts"', 10, FALSE),
-    (1, 'Click Fundamentals Button', 5, FALSE),
-    (1, 'Play "Universal Paperclips"', 5, FALSE),
-    (1, 'Complete STEM Quiz', 15, FALSE),
-    (1, 'Watch STEM Webinar', 20, FALSE),
-    (1, 'Submit a STEM Project', 25, FALSE),
-    (1, 'Join STEM Community Forum', 10, FALSE),
-    (1, 'Share STEM Article on Social Media', 5, FALSE);
