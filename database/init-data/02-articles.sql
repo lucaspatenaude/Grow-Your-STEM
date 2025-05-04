4ca4fcf0639c577dc3fb07346440e2b49d691a28
Lucas Patenaude <LUPA7501@colorado.edu>
2025-05-03 23:56:09 -0600

    Add article thumbnails

--- database/init-data/02-articles.sql
+++ database/init-data/02-articles.sql
@@ -26,10 +26,10 @@ INSERT INTO articles (AuthorID, Title, Summary, Link, ImagePath, ImageAlt)
 VALUES
     
     (1, 'Credit and Financing Options', 'Navigating funding and credit options for education can be daunting. This article explores popular options for STEM majors', '/articles/lucas/credit-and-financing-options', '/img/articles/Lucas/Credit-and-Finance-Options.jpeg', 'Sample Event Image'),
-    (1, 'The Future of AI in the Workplace', 'An in-depth analysis on what the effects of current-day tariffs look like', '/articles/lucas/future-of-ai-in-the-workplace', '/img/articles/Lucas/Future-of-AI-in-the-Workplace.jpeg', 'Sample Event Image'),
+    (1, 'The Future of AI in the Workplace', 'An in-depth analysis on what the effects of current-day tariffs look like', '/articles/lucas/future-of-ai-in-the-workplace', '/img/articles/Lucas/Future-of-AI-in-the-Workplace.jpg', 'Sample Event Image'),
     (1, 'Investment Trends in 2025', 'Summary', '/articles/lucas/investment-trends-2025', '/img/articles/Lucas/Investment-Trends-2025.jpg', 'Sample Event Image'),
-    (1, 'The Dangers of Modern Investment Platforms', 'Summary', '/articles/lucas/dangers-of-modern-investment-platforms', '/img/articles/Lucas/The-Dangers-of-Modern-Investment-Platforms.jpg', 'Sample Event Image'),
-    (1, 'Popularity of Options Financing', 'Summary', '/articles/lucas/popularity-of-options-financing', '/img/articles/Lucas/opularity-of-Options-Financing.jpg', 'Sample Event Image'  ),
+    (1, 'The Dangers of Modern Investment Platforms', 'Summary', '/articles/lucas/dangers-of-modern-investment-platforms', '/img/articles/Lucas/Dangers-of-Modern-Investment-Platforms.jpg', 'Sample Event Image'),
+    (1, 'Popularity of Options Trading', 'Summary', '/articles/lucas/popularity-of-options-trading', '/img/articles/Lucas/Popularity-of-Options-Trading.jpg', 'Sample Event Image'  ),
     (2, 'How do Tariffs Work?', 'An in-depth analysis on what the effects of current-day tariffs look like', '/articles/nathaniel/how-do-tariffs-work', '/img/articles/Nathaniel/How-Do-Tariffs-Work.jpg', 'Sample Event Image'),
     (3, 'Retirement Accounts: How Do They Work?', 'Saving money at a young age can seem daunting but the benefits are far reaching', '/articles/clay/retirement-accounts', '/img/articles/Clay/Retirement-Accounts.jpg', 'Sample Event Image'),
     (3, 'The US Dollar as the Global Reserve Currency.', 'The United States holds a lot of power as the nation behind the global reserve currency, but what does it mean for us? What happens if we lose it?', '/articles/clay/global-reserve', '/img/articles/Clay/Retirement-Accounts.jpg', 'Sample Event Image'),
