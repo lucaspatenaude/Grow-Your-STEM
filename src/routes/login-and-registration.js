ac5c2e8f0dc4c9709775462150f882e1034d5aae
Lucas Patenaude <LUPA7501@colorado.edu>
2025-05-04 19:20:50 -0600

    Add Clay's "Crypto" article

--- src/routes/login-and-registration.js
+++ src/routes/login-and-registration.js
@@ -94,7 +94,7 @@ router.post("/register", async (req, res) => {
             { id: 3, name: 'Read Article "Retirement Accounts"', points: 30, location: '/articles/Clay/retirement-accounts' },
             { id: 4, name: 'Read Article "Global Reserve"', points: 30, location: '/articles/Clay/global-reserve'},
             { id: 5, name: 'Read Article "What\'s a Negative Price?"', points: 20, location: '/articles/Clay/whats-a-negative-price' },
-            { id: 6, name: 'Read Article "Meme Stocks & Cryptocurrency"', points: 20, location: '/articles/Clay/meme-stocks-and-crypto' },
+            { id: 6, name: 'Read Article "Why YOU care about Crypto Currency!"', points: 20, location: '/articles/Clay/crypto' },
             { id: 7, name: 'Read Article "Future of AI in the Workplace"', points: 20, location: '/articles/lucas/future-of-ai-in-the-workplace' },
             { id: 8, name: 'Read Article "Investment Trends of 2025"', points: 50, location: '/articles/lucas/investment-trends-2025' },
             { id: 9, name: 'Read Article "Dangers of Modern Investment Platforms"', points: 20, location: '/articles/Lucas/dangers-of-modern-investment-platforms' },
